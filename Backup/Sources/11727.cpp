0fde69719252e7b2da442fdbbe260db4330e257c
unknown <newjincar33@gmail.com>
2021-07-24 16:51:13 +0900

    11727

---
+++ Backup/Sources/11727.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000] = { 0, 1, 3 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++)
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+	cout << dp[n] << endl;
+
+	return 0;
+}
