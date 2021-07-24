b10805a6ea616a1af49e8780f222483c630879bd
unknown <newjincar33@gmail.com>
2021-07-24 16:54:37 +0900

    9095

---
+++ Backup/Sources/9095.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[11] = { 0, 1, 2, 4 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int n;
+		cin >> n;
+		for (int j = 4; j <= n; j++)
+			dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+		cout << dp[n] << endl;
+	}
+
+	return 0;
+}
