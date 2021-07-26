e71be804c15e9ccab875000bd7d07afe058b7f6a
unknown <newjincar33@gmail.com>
2021-07-27 00:14:12 +0900

    9465 변수명 수정

--- Solved/Sources/9465/9465.cpp
+++ Solved/Sources/9465/9465.cpp
@@ -23,9 +23,9 @@ int main() {
 		dp[0][1] += dp[1][0];
 		dp[1][1] += dp[0][0];
 
-		for (int i = 2; i < n; i++) {
-			dp[0][i] += max(dp[1][i - 2], dp[1][i - 1]);
-			dp[1][i] += max(dp[0][i - 2], dp[0][i - 1]);
+		for (int j = 2; j < n; j++) {
+			dp[0][j] += max(dp[1][j - 2], dp[1][j - 1]);
+			dp[1][j] += max(dp[0][j - 2], dp[0][j - 1]);
 		}
 
 		cout << max(dp[0][n - 1], dp[1][n - 1]) << endl;
