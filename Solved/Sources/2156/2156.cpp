6ac6467e65cf45e8352d5d05b9835b055a2da014
unknown <newjincar33@gmail.com>
2021-07-27 04:16:19 +0900

    2156

---
+++ Solved/Sources/2156/2156.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[3][10001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> dp[1][i];
+		dp[2][i] = dp[1][i];
+	}
+
+	for (int i = 2; i <= n; i++) {
+		dp[0][i] = max(dp[0][i - 1], max(dp[1][i - 1], dp[2][i - 1]));
+		dp[1][i] += dp[0][i - 1];
+		dp[2][i] += dp[1][i - 1];
+	}
+
+	cout << max(dp[0][n], max(dp[1][n], dp[2][n])) << endl;
+
+	return 0;
+}
