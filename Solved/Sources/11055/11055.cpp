d375a17b0abef7965d7a16fc5042c9441de11b8b
Shin Jinhyeok <newjincar33@gmail.com>
2021-07-27 07:13:22 +0900

    11055

---
+++ Solved/Sources/11055/11055.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[1001];
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		dp[i] = a[i];
+	}
+
+	for (int i = 2; i <= n; i++) {
+		int max = 0;
+		for (int j = 1; j < i; j++)
+			if (a[i] > a[j] && max < dp[j])
+				max = dp[j];
+		dp[i] += max;
+	}
+
+	cout << *max_element(&dp[1], &dp[n + 1]) << endl;
+
+	return 0;
+}
