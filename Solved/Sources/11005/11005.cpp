6c3d8b82f2a22e872c6a1d7b088046f3b93985fd
Shin Jinhyeok <newjincar33@gmail.com>
2021-07-29 21:01:37 +0900

    11005

---
+++ Solved/Sources/11005/11005.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+char digit[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void convert(int n, int b) {
+	if (!n)
+		return;
+
+	convert(n / b, b);
+	cout << digit[n % b];
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, b;
+	cin >> n >> b;
+
+	convert(n, b);
+
+	return 0;
+}
