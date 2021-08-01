bd7fb1610f4c0108ce54ec617d30baaaabc28e3a
Shin Jinhyeok <newjincar33@gmail.com>
2021-08-02 00:08:01 +0900

    1967

---
+++ Solved/Sources/1967/1967.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> adj[100001];
+bool checked[2][100001];
+int max_distance[2], max_node[2];
+
+void dfs(int node, int sum, int index) {
+	if (checked[index][node])
+		return;
+
+	checked[index][node] = true;
+
+	if (sum > max_distance[index]) {
+		max_distance[index] = sum;
+		max_node[index] = node;
+	}
+
+	for (auto adj_node : adj[node])
+		dfs(adj_node.first, sum + adj_node.second, index);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n - 1; i++) {
+		int from, to, weight;
+		cin >> from >> to >> weight;
+
+		adj[from].push_back({ to, weight });
+		adj[to].push_back({ from, weight });
+	}
+
+	dfs(1, 0, 0);
+	dfs(max_node[0], 0, 1);
+
+	cout << max_distance[1] << endl;
+
+	return 0;
+}
