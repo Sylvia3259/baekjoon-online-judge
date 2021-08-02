bb6af40dfe4984246983947e179d7ceaa9fefbc2
Shin Jinhyeok <newjincar33@gmail.com>
2021-08-02 13:12:06 +0900

    1654

---
+++ Solved/Sources/1654/1654.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+long long lan[10000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int k, n;
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++)
+		cin >> lan[i];
+
+	long long left = 1;
+	long long right = LLONG_MAX - 1;
+
+	while (left <= right) {
+		long long length = (left + right) / 2;
+
+		int count = 0;
+		for (int i = 0; i < k; i++)
+			count += lan[i] / length;
+
+		if (count < n)
+			right = length - 1;
+		else if (count >= n)
+			left = length + 1;
+	}
+
+	cout << right << endl;
+
+	return 0;
+}
