50d6d2bfe69e0f9c72108d893817bac5612758df
Shin Jinhyeok <newjincar33@gmail.com>
2021-08-02 17:52:52 +0900

    10815

---
+++ Solved/Sources/10815/10815.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int arr[500000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+
+	int m;
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		int value;
+		cin >> value;
+
+		cout << binary_search(arr, arr + n, value) << ' ';
+	}
+
+	return 0;
+}
